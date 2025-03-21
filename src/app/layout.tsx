cb8e9e87d6d4507a6a2575309142e21ed432e243
vickydartech <73622790+vickyalvandob@users.noreply.github.com>
2025-03-21 17:13:33 +0700

    add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import Navbar from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,11 +28,32 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
+           <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="min-h-screen">
+              <Navbar />
+            <main className="py-8">
+              <div className="max-w-7xl mx-auto px-4">
+                <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                  <div className="hidden lg:block lg:col-span-3">
+                    sidebar
+                  </div>
+                  <div className="lg:col-span-9">
+                  {children}
+                  </div>
+                </div>
+              </div>
+            </main>
+            </div>
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-    <SignedOut>
-      <SignInButton mode="modal" />
-      <SignUpButton />
-    </SignedOut>
-    <SignedIn>
-      <UserButton />
-    </SignedIn>
+      <h1>content</h1>
     </div>
   );
 }
