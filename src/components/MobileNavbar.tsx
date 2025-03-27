cc45f185666663babcb4a9804b1ffb704aa1eb2a
vickyalvandob <73622790+vickyalvandob@users.noreply.github.com>
2025-03-27 10:28:55 +0700

    profile page and upload image

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -12,7 +12,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { useState } from "react";
-import { useAuth, SignInButton, SignOutButton } from "@clerk/nextjs";
+import { useAuth, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { useTheme } from "next-themes";
 import Link from "next/link";
 
@@ -20,6 +20,11 @@ function MobileNavbar() {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const { isSignedIn } = useAuth();
   const { theme, setTheme } = useTheme();
+  const { user } = useUser();
+  
+  const profileLink = user 
+  ? `/profile/${user.username ?? user.emailAddresses[0].emailAddress.split("@")[0]}`
+  : "/profile";
 
   return (
     <div className="flex md:hidden items-center space-x-2">
@@ -61,7 +66,7 @@ function MobileNavbar() {
                   </Link>
                 </Button>
                 <Button variant="ghost" className="flex items-center gap-3 justify-start" asChild>
-                  <Link href="/profile">
+                  <Link href={profileLink}>
                     <UserIcon className="w-4 h-4" />
                     Profile
                   </Link>
